1062cff6f611bc04c11cf22445e3aa28e15024b0
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2023-05-30 23:12:25 +0200

    Task 1

--- Program.cs
+++ Program.cs
@@ -2,9 +2,42 @@
 {
     public static class Program
     {
+        public static List<string> ListItems = new();
         public static void Main(string[] args)
         {
+            bool exit = false;
+            while (!exit)
+            {
+                System.Console.Write("ShoppingList> ");
+                string? userInput = Console.ReadLine();
+                if (userInput != "stop")
+                {
+                    AddItem(userInput);
+                }
+                else
+                {
+                    exit = true;
+                    SortList();
+                    OutputList();
+                }
+            }
+        }
 
+        public static void AddItem(string userInput)
+        {
+            ListItems.Add(userInput);
+        }
+
+        public static void SortList()
+        {
+            ListItems.Sort();
+        }
+        public static void OutputList()
+        {
+            foreach (string item in ListItems)
+            {
+                System.Console.WriteLine(item);
+            }
         }
     }
 }
